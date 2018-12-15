bd4324a311fe8080c12d58d00dd15eaf54ea779b
Thomas <sang4@illinois.edu>
2018-12-15 17:35:40 -0500

    finished question2 and updated readme

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 1. The function is inside question1.pl file. You can change the arguments $password to test the check_password function.
 Assuming you have perl 5.0 installed, you can run "perl .\question1.pl" to test out the function.
 
-2. 
+2. The executable is already in the folder, double click on it to run and input your expression to test. 
+Enter "exit" to exit the executable.
--- question2.py
+++ question2.py
@@ -1,14 +1,102 @@
-ops = {"+", "-", "/", "*"}
+ops = {"+": 1, "-": 1, "/":2, "*":2, "empty": 0}
 
-def removeParenthesis(s):
-    paranthesis = []
-    operator = []
-    for i in range(len(s)):
+# checks if the two paranthesis at start and end are redundant
+def ifRedundant(left, right, start, end, s, redun):
+    if left is None and right is None:
+        redun.append(start)
+        redun.append(end)
+        return
+    else:
+        if left is None:
+            left = "empty"
+            left
+        if right is None:
+            right = "empty"
+        x = findLowest(start, end, s)
+        if x is None:
+            redun.append(start)
+            redun.append(end)
+            return
+        # special case where the paranthesis are both necessary when there's subtraction before and inside the paranthesis
+        if left == "-" and (s[x] == '-' or s[x] == '+'):
+            return
+        if ops[s[x]] < ops[left] or ops[s[x]] < ops[right]:
+            return
+        else:
+            redun.append(start)
+            redun.append(end)
+            return
+
+# finds the lowest operator inside the two paranthesis
+def findLowest(start, end, s):
+    lowest = []
+    flag = False
+    for i in range(start+1, end):
+        if s[i] == "(":
+            flag = True
+        elif s[i] in ops and flag == False:
+            lowest.append((i, ops[s[i]]))
+        elif s[i] == ")":
+            flag = False
+    if len(lowest) == 0:
+        return None
+    else:
+        low = min(lowest, key=lambda x: x[1])
+        return low[0]
+
+# finds the first operator to the left of the starting paranthesis
+def findLeft(index, s):
+    left = None
+    for i in range(index, -1, -1):
         if s[i] in ops:
-            operator.append(i)
-        elif s[i] == "(":
-            paranthesis.append(i)
-    print(paranthesis)
-    print(operator)
+            left = s[i]
+            break
+    return left
 
-removeParenthesis("(1/2)")
+# finds the first operator to the right of the ending paranthesis
+def findRight(index, s):
+    right = None
+    for i in range(index, len(s)):
+        if s[i] in ops:
+            right = s[i]
+            break
+    return right
+
+# finds the corresponding ending paranthesis
+def findEndBracket(index, s):
+    stack = []
+    for i in range(index, len(s)):
+        if s[i] == "(":
+            stack.append(i)
+        elif s[i] == ")":
+            stack.pop()
+            if len(stack) == 0:
+                return i
+
+# returns a simplified version without unnecessary paranthesis
+def removeParenthesis(s):
+    redun = []
+    for i in range(len(s)):
+        if s[i] == "(":
+            start = i
+            end = findEndBracket(i, s)
+            left = findLeft(start, s)
+            right = findRight(end, s)
+            ifRedundant(left, right, start, end, s, redun)
+    if len(redun) == 0:
+        return s
+    else:
+        simplified = ""
+        for i in range(len(s)):
+            if i in redun:
+                continue
+            else:
+                simplified += s[i]
+        return simplified
+
+while True:
+    s = input("Enter the expression: ")
+    if s == "exit":
+        break
+    else:
+        print(removeParenthesis(s))
